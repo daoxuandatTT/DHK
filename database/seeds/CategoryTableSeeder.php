4920b2cc0e676d38af9ede5f2e18f56d0be4411d
nguyenkhanhhn <nguyenkhanheng3794@gmail.com>
2019-11-06 16:29:45 +0700

    update change password profile

--- database/seeds/CategoryTableSeeder.php
+++ database/seeds/CategoryTableSeeder.php
@@ -13,17 +13,52 @@ class CategoryTableSeeder extends Seeder
     public function run()
     {
         $category = new Category();
-        $category->name = "món chay";
+        $category->name = "Canh,bún,ph";
         $category->image = "image1";
         $category->save();
 
         $category = new Category();
-        $category->name = "món mặn";
+        $category->name = "Chiên,rán,nướng";
         $category->image = "image2";
         $category->save();
 
         $category = new Category();
-        $category->name = "món ngọt";
+        $category->name = "Món hầm,ninh,om";
+        $category->image = "image3";
+        $category->save();
+
+        $category = new Category();
+        $category->name = "Món nộm,gỏi";
+        $category->image = "image3";
+        $category->save();
+
+        $category = new Category();
+        $category->name = "Món ăn chay";
+        $category->image = "image3";
+        $category->save();
+
+        $category = new Category();
+        $category->name = "Món tráng miệng";
+        $category->image = "image3";
+        $category->save();
+
+        $category = new Category();
+        $category->name = "Kho,rang,xào";
+        $category->image = "image3";
+        $category->save();
+
+        $category = new Category();
+        $category->name = "Món luộc,hấp";
+        $category->image = "image3";
+        $category->save();
+
+        $category = new Category();
+        $category->name = "Món lẩu";
+        $category->image = "image3";
+        $category->save();
+
+        $category = new Category();
+        $category->name = "Món ngon khác";
         $category->image = "image3";
         $category->save();
     }
